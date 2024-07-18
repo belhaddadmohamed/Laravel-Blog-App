aad7ed120aa4de64c2d91316a3c129affcb4b830
belhaddadmohamed <belhaddad.mohamed7@gmail.com>
2024-07-18 02:02:03 +0100

    Create Post livewire

--- app/Livewire/PostList.php
+++ app/Livewire/PostList.php
@@ -2,10 +2,19 @@
 
 namespace App\Livewire;
 
+use Livewire\Attributes\Computed;
+use App\Models\Post;
 use Livewire\Component;
 
 class PostList extends Component
 {
+    #[Computed()]
+    public function posts()
+    {
+        return Post::take(5)->get();
+    }
+
+
     public function render()
     {
         return view('livewire.post-list');
